bf59479882f44bf5f11b5585bf11ed5d39a7affe
iGalich <89650689+iGalich@users.noreply.github.com>
2022-04-27 18:44:27 +0300

    Changes

--- NewbiesGameJam/Assets/Scripts/Player/PlayerHealth.cs
+++ NewbiesGameJam/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -12,14 +13,26 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private int _currentHealth; // serialized for testing purposes
     private bool _isDead = false;
 
+    [Header ("IFrames")]
+    [SerializeField] private float _iFramesDuration = 1f;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private SpriteRenderer _grappleSprite;
+    [SerializeField] private float _iFramesDeltaTime = 0.15f;
+    private float _lastHit;
+    private WaitForSeconds _iFramesTick;
+
     [Header ("Grapple Gun")]
     [SerializeField] private GameObject _grappleGunPrefab;
     [SerializeField] private float _pushValue = 1000f;
     private GameObject _grappleGun;
 
     [Header ("Shake parameters")]
+    [SerializeField] private GameObject _healthBar;
     [SerializeField] private float _shakeIntensity = 10f;
     [SerializeField] private float _shakeTime = 0.5f;
+
+    [Header ("Particles")]
+    [SerializeField] private ParticleSystem _hitParticles;
 
     public int StartingHealth => _startingHealth;
     public int CurrentHealth => _currentHealth;
@@ -27,6 +40,11 @@ public class PlayerHealth : MonoBehaviour
     private void Awake()
     {
         _currentHealth = _startingHealth;
+    }
+
+    private void Start()
+    {
+        _iFramesTick = new WaitForSeconds(_iFramesDeltaTime);
     }
 
     private void Update()
@@ -47,20 +65,38 @@ public class PlayerHealth : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
+        if (Time.time - _lastHit <= _iFramesDuration) return;
+
         Debug.Log(this.name + " took " + damage + " damage");
 
         _currentHealth -= damage;
 
         if (_currentHealth > 0)
         {
+            _lastHit = Time.time;
+            StartCoroutine(StartIFrames());
             OnHealthChange.Invoke();
-            GameManager.Instance.cinemachineShake.ShakeCamera(_shakeIntensity, _shakeTime);
+            _hitParticles.Play();
+            FunctionTimer.Create(() => _hitParticles.Stop(), _hitParticles.main.duration);
+            iTween.ShakePosition(_healthBar, Vector3.one * _shakeIntensity, _shakeTime);
         }
         else if (!_isDead)
         {
             OnPlayerDeath.Invoke();
             LaunchGrappleGun();
         }
+    }
+
+    private IEnumerator StartIFrames()
+    {
+        while (Time.time - _lastHit <= _iFramesDuration)
+        {
+            _spriteRenderer.enabled = !_spriteRenderer.enabled;
+            _grappleSprite.enabled = !_grappleSprite.enabled;
+            yield return _iFramesTick;
+        }
+        _spriteRenderer.enabled = true;
+        _grappleSprite.enabled = true;
     }
     
     // Disables and spawns a new grapple gun, which flies off in a random direction
